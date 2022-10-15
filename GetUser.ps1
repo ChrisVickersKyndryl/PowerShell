cd468acdeebbe1c6af2d8a18eebb6b97f7632463
ChrisVickers1985 <46434436+ChrisVickers1985@users.noreply.github.com>
2022-10-15 21:27:15 +1100

    Create GetUser.ps1

---
+++ GetUser.ps1
@@ -0,0 +1,17 @@
+# Get folder size and output as a table
+$list = New-Object Collections.Generic.List[string]
+gci -force 'C:\Users'-ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % {
+  $len = 0
+  gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }
+  $list.Add([string]$len)
+  $list.Add($_.fullname) 
+  $list.Add($_.name) 
+}
+$list | Format-Wide -Property {$_} -Column 3 -Force
+
+# echo $list.Count
+
+# Get user details for each folder
+for($i=2; $i -lt $list.Count; $i+=3) {
+  echo $list[$i]
+}
