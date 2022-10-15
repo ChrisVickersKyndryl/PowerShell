346644d510a46f437b4775db274e0b908cf6406b
ChrisVickers1985 <46434436+ChrisVickers1985@users.noreply.github.com>
2022-10-15 23:53:30 +1100

    Update GetUser.ps1

--- GetUser.ps1
+++ GetUser.ps1
@@ -1,6 +1,8 @@
+#https://www.jonathanmedd.net/2019/07/returning-data-from-powershell-scripts-to-be-consumed-by-ansible-playbooks.html
+
 # Check if module exists
 if (Get-Module -ListAvailable -Name ActiveDirectory) {
-  # Get list variable
+  # List of folders with user details
   $listOfObj = New-Object -TypeName 'System.Collections.ArrayList';
 
   # Get folder size and output as a table
@@ -15,15 +17,23 @@ if (Get-Module -ListAvailable -Name ActiveDirectory) {
   }
 
   foreach ($i in $array){
+    # Delete deactivated users
     Get-ADUser -Filter "Name -eq $i.name" -SearchBase "DC=AppNC" | foreach {
       $i.enabled = $_.Enabled
+      $i.email = $_.Email
+      $i.name = $_.Name
     
       # DELETES FILES IF THE USER IS DISABLED
       # if ($i.enabled -eq "False") {
       #   Remove-Item 'D:\temp\Test Folder' -Recurse
       #}
     }
+    
+    # Check if folder is too large
+    $i.too_large = ($i.size -lt 20000000) ? "True" : "False"
   }
+  
+  
 } 
 else {
     Write-Host "Module does not exist"
