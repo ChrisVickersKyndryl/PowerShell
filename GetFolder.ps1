95ac672157ef83cf3483132bca70146fa0251e82
ChrisVickers1985 <46434436+ChrisVickers1985@users.noreply.github.com>
2022-10-16 15:18:35 +1100

    Update GetFolder.ps1

--- GetFolder.ps1
+++ GetFolder.ps1
@@ -1,5 +1,12 @@
 #https://www.jonathanmedd.net/2019/07/returning-data-from-powershell-scripts-to-be-consumed-by-ansible-playbooks.html
 
+# Check if module exists. If it does not, exit
+if (!(Get-Module -ListAvailable -Name ActiveDirectory))
+{
+  Write-Host "Module does not exist"
+  return
+}
+
 # List of folders with user details
 $listOfObj = New-Object -TypeName 'System.Collections.ArrayList';
 
@@ -15,15 +22,8 @@ $excludedFolders = @(
   "Default User"
 )
 
-# This gets the current time as a string
+# This gets the current time as a string that can be compared to the time returned from AD
 $dateTwoMonthsAgo = ((Get-Date).AddMonths(-2)).tofiletime()
-
-# Check if module exists. If it does not, exit
-if (!(Get-Module -ListAvailable -Name ActiveDirectory))
-{
-  Write-Host "Module does not exist"
-  return
-}
 
 # Get folder size and name
 gci -force 'C:\Users'-ErrorAction SilentlyContinue |
